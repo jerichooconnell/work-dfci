571f2642df7ca2fee7251fcf6e1329fbc4ed6420
Jericho O'Connell <jericho.oconnell@gmail.com>
2024-06-07 13:47:59 -0400

    Generation of head data

---
+++ 7-Simulate_Patients_Matt/Head/1-Jupyter_notebooks/gecco_scripts_matlab/Patient_Head_Male_07.py
@@ -0,0 +1,79 @@
+
+import numpy as np
+from gecco import patient_data, calculate_spectrum_sp
+from gecco.utils import nrrd_to_mhd
+from scipy.io import savemat
+import os
+
+phantom = patient_data.patient_phantom('/home/berbecolab/2-Datadir/Clinical_data/Head/Patient_Head_Male_07.nrrd', 1e10, reload=True, sim_num=None)
+
+# Source spectrum
+spectrum = calculate_spectrum_sp(140,12)
+
+# Beam filters
+spectrum.filter('Be', 1.5)
+spectrum.filter('Al', 2.75)
+spectrum.filter('Ti', 0.89)
+
+# Detector filters
+spectrum.filter('Al', 3.7)
+spectrum.filter('C', 3.8*(2/1.7)) # Difference in density
+
+phantom.xx, phantom.yy = spectrum.get_points()
+phantom.run_gecco(1e20,360,conv_on=False)
+
+phantom.correct_intensity(ml=False,crop=40)
+phantom.interpolate_ggems_scatter()
+phantom.calc_gecco_projections(scat_weight=0.48,noise_weight=100) # Creates the gecco projections scatter to primary reduction from asg is 0.48 assumed half is taken by scatter correction
+
+# --------------------- Second Layer ----------------
+
+phantom2 = patient_data.patient_phantom('/home/berbecolab/2-Datadir/Clinical_data/Head/Patient_Head_Male_07.nrrd', 1e10, reload=True, sim_num=None, second_layer=True)
+
+# Source spectrum
+spectrum = calculate_spectrum_sp(140,12)
+
+# Beam filters
+spectrum.filter('Be', 1.5)
+spectrum.filter('Al', 2.75)
+spectrum.filter('Ti', 0.89)
+
+# Detector filters
+spectrum.filter('Al', 3.7)
+spectrum.filter('C', 3.8*(2/1.7)) # Difference in density
+
+# First layer filters
+spectrum.filter('Cesium Iodide',0.6*0.71) # 71% fill factor
+spectrum.filter('Si',1.1)
+
+phantom2.run_gecco(1e20,360,conv_on=False)
+
+phantom2.correct_intensity(ml=False,crop=40)
+phantom2.interpolate_ggems_scatter()
+phantom2.calc_gecco_projections(scat_weight=0.48,noise_weight=100)
+
+nrrd_base = 'Patient_Head_Male_07'
+
+out = '/home/berbecolab/1-Workspace/1-Workspace/7-Simulate_Patients_Matt/Head/1-Jupyter_notebooks/gecco_data_matlab'
+
+if not os.path.exists(os.path.join(out,nrrd_base)):
+    os.makedirs(os.path.join(out,nrrd_base))
+
+savemat(os.path.join(out,nrrd_base, nrrd_base + '_scatter_first_layer' + '.mat'),
+        { "Patient_Head_Male_07": phantom.ggems_scatter_denoised })
+savemat(os.path.join(out,nrrd_base, nrrd_base + '_scatter_second_layer'+ '.mat'),
+        { "Patient_Head_Male_07": phantom2.ggems_scatter_denoised })
+
+savemat(os.path.join(out,nrrd_base, nrrd_base + '_primary_first_layer'+ '.mat'),
+        { "Patient_Head_Male_07": phantom.primary_projections })
+savemat(os.path.join(out,nrrd_base, nrrd_base + '_primary_second_layer'+ '.mat'),
+        { "Patient_Head_Male_07": phantom2.primary_projections })
+
+savemat(os.path.join(out,nrrd_base, nrrd_base + '_flood_first_layer'+ '.mat'),
+        { "Patient_Head_Male_07": phantom.flood_field })
+savemat(os.path.join(out,nrrd_base, nrrd_base + '_flood_second_layer'+ '.mat'),
+        { "Patient_Head_Male_07": phantom2.flood_field })
+
+with open(os.path.join(out, nrrd_base, nrrd_base + "_simulation_info.txt"), 'w') as f:
+            f.write(str(phantom))
+
---
+++ 7-Simulate_Patients_Matt/Head/1-Jupyter_notebooks/test_python_scripts/Patient_Head_Male_03_test.py
@@ -0,0 +1,32 @@
+
+import numpy as np
+from gecco import patient_data, calculate_spectrum_sp
+from gecco.utils import nrrd_to_mhd
+
+nrrd_file = '/home/berbecolab/2-Datadir/Clinical_data/Head/Patient_Head_Male_03.nrrd'
+
+# Write to mhd
+
+nrrd_to_mhd(nrrd_file,force=True,tr= [2, 1, 0],crop=[None, None, None, None, None, None])
+
+phantom = patient_data.patient_phantom(nrrd_file, 1000000.0, force_materials=False, is_fullfan=True)
+
+# Pixel size
+phantom.geomet.nDetector = np.array([360,360])
+phantom.geomet.dDetector = np.array([1.12,1.12])
+phantom.geomet.sDetector = phantom.geomet.dDetector*phantom.geomet.nDetector
+
+# Spectrum
+spectrum = calculate_spectrum_sp(125,12)
+spectrum.filter('Al', 2.75)
+spectrum.filter('Be', 1.5)
+spectrum.filter('Ti', 0.89)
+
+phantom.initialize_fastmc(10,spectrum)
+
+phantom.run_fastmc(fastmc_path ='/home/berbecolab/Software/gecco-master/extra/fastmc_install/bin/FastMC')
+
+phantom.load_ggems()
+
+print()
+print('Done - Completed without errors')
